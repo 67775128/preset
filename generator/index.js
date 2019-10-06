3ebaa1bb076679e469ed6581f4d99a02ae3183d2
xiangdan1 <xiangdan1@vipkid.com.cn>
2019-10-06 23:28:27 +0800

    init

--- generator/index.js
+++ generator/index.js
@@ -1,6 +1,14 @@
 module.exports = (api, options, rootOptions) => {
     // 修改 `package.json` 里的字段
+  if (options.mockjs) {
+    api.extendPackage({
+      devDependencies: {
+        "mockjs": "^1.0.1-beta3"
+      }
+    });
+  }
     // 复制并用 ejs 渲染 `./template` 内所有的文件
+    
     api.render('./template')
     console.log(options, rootOptions)
   }
--- prompts.js
+++ prompts.js
@@ -1,7 +1,6 @@
 module.exports = [{
-    when: answers => answers.import === 'full',
     type: 'confirm',
-    name: 'customTheme',
-    message: 'Do you wish to overwrite Element\'s SCSS variables?',
-    default: false,
+    name: 'mockjs',
+    message: 'Do you wish to install mockjs?',
+    default: false
   }]
